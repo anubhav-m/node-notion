85591ea6dd0712909a3d657fabd3e257b75894bf
Anubhav Mishra <mishra.anubhav108@gmail.com>
2025-07-27 01:15:06 +0530

    initialized express server with mongodb

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+import { connectToDB } from './database/mongodb.js'
+import { PORT, NODE_ENV } from './config/env.js'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: false })); // Middleware to parse URL-encoded bodies
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to NodeNotion api')
+});
+
+(async () => {
+    try {
+        await connectToDB();
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT} in ${NODE_ENV} mode`);
+        });
+    }
+    catch (err) {
+        console.error(`Failed to start server: ${err.message}`);
+    }
+})();
