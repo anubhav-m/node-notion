e36f000b1b04a4e89b9dd2c1ad2b916311614c7c
Anubhav Mishra <mishra.anubhav108@gmail.com>
2025-07-31 19:43:06 +0530

    set protected route for dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from './pages/Dashboard.jsx'
 import Projects from './pages/Projects.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
 
 export default function App() {
 	return (
@@ -18,7 +19,9 @@ export default function App() {
 					<Route path='/about' element={<About />}></Route>
 					<Route path='/sign-up' element={<SignUp />}></Route>
 					<Route path='/sign-in' element={<SignIn />}></Route>
-					<Route path='/dashboard' element={<Dashboard />}></Route>
+					<Route element={<PrivateRoute/>}>
+						<Route path='/dashboard' element={<Dashboard />}></Route>
+					</Route>
 					<Route path='/projects' element={<Projects />}></Route>
 				</Routes>
 				<Footer />
