66d828f15b9a6a28a5aa86df8a1443883d21ba87
Anubhav Mishra <mishra.anubhav108@gmail.com>
2025-08-04 18:04:53 +0530

    create UI of createPost

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,13 +1,15 @@
-import { TextInput, Select } from 'flowbite-react'
+import { TextInput, Select, FileInput } from 'flowbite-react'
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 
 export default function CreatePost() {
     return (
-        <div className="flex-1 w-full md:w-200 self-center px-6">
+        <div className="flex-1 w-full md:max-w-5xl self-center px-6">
             <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
 
             <form className="flex flex-col gap-4">
-                <div className="flex flex-col md:flex-row gap-4 justify-between">
-                    <TextInput type='text' placeholder='Title' required id='title' className='flex-1'/>
+                <div className="flex flex-col md:flex-row gap-4 justify-between mb-5">
+                    <TextInput type='text' placeholder='Title' required id='title' className='flex-1' />
                     <Select>
                         <option value="uncategorized">Select a category</option>
                         <option value="javascript">JavaScript</option>
@@ -16,6 +18,23 @@ export default function CreatePost() {
 
                     </Select>
                 </div>
+
+                <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3 mb-5">
+                    <FileInput type='file' accept='image/*'></FileInput>
+                    <button class="cursor-pointer relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                            Upload Image
+                        </span>
+                    </button>
+                </div>
+
+                <ReactQuill theme='snow' className='h-72 mb-15' required/>
+
+                <button class="cursor-pointer relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+                    <span class="w-full relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                        Publish
+                    </span>
+                </button>
             </form>
         </div>
     )
